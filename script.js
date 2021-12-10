a0d80a61156031131248c0c6191e8d8dd1c5211d
reneehoh <56978514+reneehoh@users.noreply.github.com>
2021-12-10 03:27:14 -0500

    scrambling works

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+
 var ponder = $(".ponder-btn");
 var container = $('.formContainer');
 var apiKey = "";
+const letterAureliano = document.getElementById('letterAureliano');
+const letterNarrator = document.getElementById('letterNarrator');
+const letterWenjie = document.getElementById('letterWenjie');
+// var letterAurelianoOpen = false;
+
 
 // CREATE NOTE
 $("#ponder-btn").click(function () {
@@ -12,36 +18,55 @@ $("#ponder-btn").click(function () {
     }
 })
 
-// OPEN NOTE
+// HOVER OVER NOTE
+const letterHover = [letterAureliano, letterNarrator, letterWenjie];
+letterHover.forEach((d, i) => {
+    d.addEventListener("mouseenter", function( event ) {
+        if (d.src.match("images/letter-closed.png")) {
+            d.src = "images/letter-open.png";
+        }
+    })
+    d.addEventListener("mouseout", function( event ) {
+        if (d.src.match("images/letter-open.png")) {
+            d.src = "images/letter-closed.png";
+        }
+    });
+})
 
+// OPEN NOTE
 $("#letterAureliano").click(function () {
     refresh();
-   var message = "when my father took me to discover ice"
+   var message = "When my father took me to discover ice."
    open(message);
-   imgChange('letterAureliano');
-   document.getElementById("regards").innerHTML = "with gratitude, aureliano";
-
+   var imageID = document.getElementById('letterAureliano');
+//    if (imageID.src.match("images/letter-closed.png")) {
+    imageID.src = "images/letter-open.png";
+    // }  
+    document.getElementById("regards").innerHTML = "With Gratitude, Aureliano";
 })
 
 $("#letterNarrator").click(function () {
     refresh();
-    var message = "a somber farewell to my shadow"
+    var message = "A single white bird taking flight. The bird wings over the Wall and into the flurried clouds of the southern sky."
     open(message);
     imgChange('letterNarrator');
-    document.getElementById("regards").innerHTML = "";
+    document.getElementById("regards").innerHTML = "Warmly, The Dreamreader";
 })
 
 $("#letterWenjie").click(function () {
     refresh();
-    var message = "when my father took me to discover ice"
+    var message = "Any idea that dared to take flight would only crash back to the ground. The gravity of reality is too strong."
     open(message);
     imgChange('letterWenjie');
+    document.getElementById("regards").innerHTML = "Regards, Ye Wenjie";
 })
-
 
 // HELPER - OPEN EXISTING NOTE
 function open(message) {
-    var words = message.split(" ");
+    // var removePunc = message.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var removePunc = message;
+
+    var words = removePunc.split(" ");
     words.forEach((d,i) => {
         const newWord = document.createElement("span");
         newWord.setAttribute("id", d);
@@ -50,6 +75,7 @@ function open(message) {
         newWord.appendChild(node0);
         const element0 = document.getElementById("thoughts");
         element0.appendChild(newWord);
+        
 
         newWord.addEventListener("mouseenter", function( event ) {
             document.getElementById("wordName").innerHTML = "";
@@ -57,8 +83,24 @@ function open(message) {
             document.getElementById("wordSpeech").innerHTML = "";
             document.getElementById("wordOff").innerHTML = "";
             
-            define(d);
+            define(newWord.id, false, d);
+            console.log(newWord.id)
         });
+
+        // SCRAMBLE DEFINITION WHEN CLICK ON WORD
+        newWord.addEventListener("click", function( event ) {
+            // document.getElementById("wordName").innerHTML = "";
+            document.getElementById("wordDef").innerHTML = "";
+            document.getElementById("wordSpeech").innerHTML = "";
+            document.getElementById("wordOff").innerHTML = "";
+
+            newWord.setAttribute("class", "mouseoverScramble");
+
+            randomWord = $.getJSON(`https://random-word-api.herokuapp.com/word?number=1`, function(result){
+                newWord.setAttribute("id", result[0]);
+                define(result[0], true, d)            
+            })  
+        })
     })
 }
 
@@ -95,23 +137,29 @@ function refresh() {
 }
 
 // HELPER - SHOW DEFINITION OF WORD ON HOVER
-function define(word) {
+// word is the definition we want to use
+// name is the name we should be displaying
+function define(word, ifScramble, name) {
     wordURL = $.getJSON(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=f7e7c888-6c65-4be3-8316-37423b42381e`, function(result){
-    console.log(typeof result[0]);
-    if ((typeof result[0]) != string) {
+    if (typeof result[0] === 'object' && result[0] !== null) {
         const wordInfo = result[0]
-        const wordName = word;
+
+        var wordName = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+        wordName = wordName.toString().toLowerCase()
 
         const wordDef = wordInfo['shortdef'];
         const wordSpeech = wordInfo['fl'];
         const wordOff = wordInfo['meta']['offensive'];
 
-        const newName = document.createElement("span");
-        const node0 = document.createTextNode(wordName);
-        newName.appendChild(node0);
-        const element0 = document.getElementById("wordName");
-        element0.appendChild(newName);
-        
+        if (ifScramble === false) {
+            const newName = document.createElement("span");
+            newName.setAttribute("id", "wordNameSpan");
+            const node0 = document.createTextNode(name.toLowerCase());
+            newName.appendChild(node0);
+            const element0 = document.getElementById("wordName");
+            element0.appendChild(newName);
+        }
+
         wordDef.forEach((d,i) => {
             const newDef = document.createElement("p");
             const node = document.createTextNode(wordDef[i]);
@@ -140,11 +188,14 @@ function define(word) {
             element3.appendChild(newOff);
         }
     } else {
-        document.getElementById("wordName").innerHTML = "this defintion doesn't exist <em>yet</em>";
-
+        if (ifScramble === false) {
+            document.getElementById("wordName").innerHTML = "this defintion doesn't exist <em>yet</em>";
+        } else {
+            // CONTINUE UNTIL WORD WITH DEFINITION
+            randomWord = $.getJSON(`https://random-word-api.herokuapp.com/word?number=1`, function(result){
+                define(result[0], true, name)       
+        })    
     }
-    }) 
+    }
+}) 
 }
-
-
-
